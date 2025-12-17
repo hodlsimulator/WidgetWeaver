f4e7b8245d8d5e3f282c5414f1db8aea62c68559
Conor <hodlsimulator@gmail.com>
2025-12-17 00:20:38 +0000

    Initial Xcode project

---
+++ WidgetWeaver/README.md
@@ -0,0 +1,11 @@
+# WidgetWeaver
+
+WidgetWeaver is an iOS 26 app for building widgets from natural language (work in progress).
+
+## Status
+Early scaffolding: iOS app shell + app icon in place.
+
+## Requirements
+- Xcode 26+
+- iOS 26+
+
---
+++ WidgetWeaver/WidgetWeaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetWeaverApp.swift
+//  WidgetWeaver
+//
+//  Created by . . on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct WidgetWeaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
