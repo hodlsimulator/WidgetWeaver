28c12fee8c867d937cd68505d268bf2729ea43be
Conor <hodlsimulator@gmail.com>
2025-12-28 23:33:57 +0000

    Clock pass, bump version number.

--- Shared/WidgetWeaverWidgetKinds.swift
+++ Shared/WidgetWeaverWidgetKinds.swift
@@ -14,5 +14,5 @@ public enum WidgetWeaverWidgetKinds {
     public static let lockScreenSteps = "WidgetWeaverLockScreenStepsWidget"
     public static let homeScreenSteps = "WidgetWeaverHomeScreenStepsWidget"
 
-    public static let homeScreenClock = "WidgetWeaverHomeScreenClockWidgetV28"
+    public static let homeScreenClock = "WidgetWeaverHomeScreenClockWidgetV29"
 }
