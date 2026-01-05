2050a6a3e2b3c49992d618e738b3b2c871a70fcc
Conor <hodlsimulator@gmail.com>
2026-01-05 04:40:26 +0000

    Clock, hands design tweak.

--- Shared/WidgetWeaverWidgetKinds.swift
+++ Shared/WidgetWeaverWidgetKinds.swift
@@ -18,7 +18,7 @@ public enum WidgetWeaverWidgetKinds {
     public static let homeScreenSteps = "WidgetWeaverHomeScreenStepsWidget"
     public static let homeScreenActivity = "WidgetWeaverHomeScreenActivityWidget"
 
-    public static let homeScreenClock = "WidgetWeaverHomeScreenClockWidgetV137"
+    public static let homeScreenClock = "WidgetWeaverHomeScreenClockWidgetV138"
 
     public static let noiseMachine = "WidgetWeaverNoiseMachineWidget"
 }
